40555c5eca8874682e1cbc072f2f3bc6369802e8
zxcdsa <a1892412@nwytg.net>
2018-11-27 22:57:06 +0100

    fixed untracked files

--- Infrastructure/Handler/Employer/CreateEmployer.cs
+++ Infrastructure/Handler/Employer/CreateEmployer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using PlayTogether.Infrastructure.Commands;
+using PlayTogether.Infrastructure.Commands.Employeer;
 
 namespace PlayTogether.Infrastructure.Handler.Employer
 {
-    public class CreateEmployer : ICommandHandler<CreateEmployeer>
+    public class CreateEmployer : ICommandHandler<CreateEmpoloyeer>
     {
-        public Task HandlerAsync(CreateEmployeer command)
+
+        public Task HandlerAsync(CreateEmpoloyeer command)
         {
             throw new NotImplementedException();
         }
